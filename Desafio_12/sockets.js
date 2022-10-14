17010ff2cf077b8d6fc60c63c333bb34b7bb398b
Alexis Velázquez <alexisvelazquez2012@gmail.com>
2022-10-14 03:08:24 -0300

    Se cargaron todas las configuraciones del desafío 11

---
+++ Desafio_12/sockets.js
@@ -0,0 +1,41 @@
+import ProductContainer from './src/containers/productContainer.js';
+import MsgContainer from './src/containers/msgContainer.js';
+
+import option from './src/databases/configMariaDB.js';
+import config from './src/databases/configSQLiteDB.js';
+
+import normalizeMessages from './src/normalizer/normalizeMessages.js'
+
+const productsApi = new ProductContainer(option, 'products');
+const messagesApi = new MsgContainer(config, 'messages');
+
+const Sockets = (io) => {
+  io.on('connection', async (socket) => {
+    console.info(`\nUn cliente con el id: [${socket.id}] se ha conectado.\n`);
+
+    // carga inicial de productos
+    socket.emit('view-products', await productsApi.readProducts());
+
+    // actualizacion de productos
+    socket.on('update-product', async (product) => {
+      await productsApi.insertProduct(product);
+      io.sockets.emit('view-products', await productsApi.readProducts());
+    });
+
+    // carga inicial de mensajes
+    socket.emit('view-messages', normalizeMessages(await messagesApi.readMsgs()));
+
+    // actualizacion de mensajes
+    socket.on('new-message', async (msg) => {
+      msg.fyh = new Date().toLocaleString();
+      await messagesApi.insertMsg(msg);
+      io.sockets.emit('view-messages', normalizeMessages(await messagesApi.readMsgs()));
+    });
+
+    socket.on('disconnect', (_) => {
+      console.info(`El cliente con el id: [${socket.id}] se ha desconectado.\n`);
+    });
+  });
+};
+
+export default Sockets;
---
+++ Desafio_12/src/utils/createTableProducts.js
@@ -0,0 +1,23 @@
+import knex from 'knex';
+
+const setColumns = (table) => {
+  table.increments('id');
+  table.string('title');
+  table.float('price');
+  table.string('thumbnail');
+  return table;
+};
+
+const createTable = async (option, tableName) => {
+  const db = knex(option);
+  try {
+    await db.schema.createTable(tableName, (table) => setColumns(table));
+    console.info(`Tabla "${tableName}" creada correctamente.`);
+  } catch (error) {
+    throw error;
+  } finally {
+    db.destroy();
+  }
+};
+
+export default createTable;
